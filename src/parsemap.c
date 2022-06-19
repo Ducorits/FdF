2ea89564ced9762aae1a22ee0584a878acc9ad88
Duco Ritsema van Eck <ducorits@zonderstreepje.nl>
2022-06-19 22:46:05 +0200

    Forgot a small detail

--- src/parsemap.c
+++ src/parsemap.c
@@ -6,7 +6,7 @@
 /*   By: dritsema <dritsema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/15 13:50:16 by dritsema      #+#    #+#                 */
-/*   Updated: 2022/06/19 22:27:44 by dritsema      ########   odam.nl         */
+/*   Updated: 2022/06/19 22:45:50 by dritsema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
